98e523f5029fbdac7dc7c0be1063bbf5f5b39b1d
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-10-06 22:48:53 -0300

    initial home

---
+++ src/hooks/useGithubProfile.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { GithubProfileData } from '@/types/api/githubProfileDate'
+
+const useGitHubProfile = (username: string) => {
+  const [profileData, setProfileData] = useState<GithubProfileData | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<unknown>(null)
+
+  useEffect(() => {
+    const fetchGitHubProfile = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get(
+          `https://api.github.com/users/${username}`,
+        )
+        setProfileData(response.data)
+        setError(null)
+      } catch (err) {
+        setProfileData(null)
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (username) {
+      fetchGitHubProfile()
+    }
+  }, [username])
+
+  return { profileData, loading, error }
+}
+
+export default useGitHubProfile
