257cd5fe7ac1bfd5e59c52e2351da9a6c6e884ed
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-01-12 11:13:02 -0300

    feat: use material-nextjs/v14-appRouter instead of v13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
 import { ThemeProvider } from '@/context/themeContext'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter' // improves mui for nextjs
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter' // improves mui for nextjs
 
 const inter = Inter({ subsets: ['latin'] })
 
