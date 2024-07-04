762b3bc9e0bbe2cae83c234f525eb6cf59fd4ede
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-07-04 18:08:40 -0300

    feat: fix ts error

---
+++ src/types/api/echarts.d.ts
@@ -0,0 +1,11 @@
+// prevent ts error: Could not find a declaration file for module
+
+declare module 'echarts/i18n/langEN-obj.js' {
+  const value: any
+  export default value
+}
+
+declare module 'echarts/i18n/langPT-br-obj.js' {
+  const value: any
+  export default value
+}
