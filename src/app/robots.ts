defc19c119d67daa4423ead71abb850cfbecc523
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-07-17 15:06:34 -0300

    feat: add sitemap for pages and robot

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env'
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: `${env.SITE_URL}/sitemap.xml`,
+  }
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import { locales } from '@/dictionaries/getDictionary'
+import { env } from '@/env'
+import { MetadataRoute } from 'next'
+
+type PageMap = {
+  url: string
+  lastModified?: string | Date
+  changeFrequency?:
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never'
+  priority?: number
+}
+
+const addLanguage = (pageMap: PageMap): MetadataRoute.Sitemap =>
+  locales.map((lang) => {
+    const urlSplit = pageMap.url.split('/')
+    urlSplit.splice(3, 0, lang)
+    const urlWithLang = urlSplit.join('/')
+
+    return {
+      url: urlWithLang,
+      lastModified: pageMap.lastModified,
+      changeFrequency: pageMap.changeFrequency,
+      priority: pageMap.priority,
+    }
+  })
+
+const makeUrlMap = () => {
+  const urlMap: MetadataRoute.Sitemap = []
+  urlMap.push(
+    ...addLanguage({
+      url: env.SITE_URL,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    }),
+  )
+  return urlMap
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteMap = makeUrlMap()
+  return siteMap
+}
