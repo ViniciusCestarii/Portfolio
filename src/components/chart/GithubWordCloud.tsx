41aa5ff8fbcb983199e596e25eeeb8ba5b208683
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-07-19 09:25:42 -0300

    feat: check performace removing heavy client side rendering

--- src/components/chart/GithubWordCloud.tsx
+++ src/components/chart/GithubWordCloud.tsx
@@ -1,18 +1,10 @@
 import { DictionaryProps } from '@/dictionaries/getDictionary'
-import wordCloudData from '@/json/githubWordCloud.json'
-import dynamic from 'next/dynamic'
-
-const GithubWordCloudClient = dynamic(
-  () => import('@/components/chart/GithubWordCloudClient'),
-  { ssr: false, loading: () => <div style={{ height: '600px' }} /> },
-)
-
 interface GithubWordCloudProps extends DictionaryProps {
   data?: { name: string; value: number }[]
 }
 
 const GithubWordCloud = async ({ data, ...props }: GithubWordCloudProps) => {
-  return <GithubWordCloudClient {...props} data={data ?? wordCloudData} />
+  return null
 }
 
 export default GithubWordCloud
--- src/components/icons/animated-icons/DynamicAnimatedIconInHover.tsx
+++ src/components/icons/animated-icons/DynamicAnimatedIconInHover.tsx
@@ -1,13 +1,7 @@
-'use client'
 import { AnimatedIconOnHoverProps } from '@/types/layout/AnimatedIcon'
-import dynamic from 'next/dynamic'
-
-const AnimatedIconOnHover = dynamic(() => import('./AnimatedIconOnHover'), {
-  ssr: false,
-})
 
 const DynamicAnimatedIconOnHover = (props: AnimatedIconOnHoverProps) => {
-  return <AnimatedIconOnHover {...props} />
+  return null
 }
 
 export default DynamicAnimatedIconOnHover
