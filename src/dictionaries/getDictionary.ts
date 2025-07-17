018c4dafd141130d04ec44732cf25e92132d38c0
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-07-17 20:41:11 -0300

    feat: improve getDictionary logic

--- src/dictionaries/getDictionary.ts
+++ src/dictionaries/getDictionary.ts
@@ -1,20 +1,28 @@
 import 'server-only'
 import { DictionaryType } from './dictionary'
 
-const dictionaries = {
-  en: () => import('./locales/en').then((module) => module.default),
-  es: () => import('./locales/es').then((module) => module.default),
-  fr: () => import('./locales/fr').then((module) => module.default),
-  pt: () => import('./locales/pt').then((module) => module.default),
-}
+export const locales = ['en', 'es', 'fr', 'pt'] as const
 
-export const locales = Object.keys(dictionaries)
-
-export type DictionaryKeyType = keyof typeof dictionaries
+export type DictionaryKeyType = (typeof locales)[number]
 
 export type DictionaryProps = {
   dict: DictionaryType
 }
 
-export const getDictionary = async (locale: DictionaryKeyType) =>
-  dictionaries[locale]()
+const getDictionaries = async () => {
+  const dictionaries = await Promise.all(
+    locales.map((lang) =>
+      import(`./locales/${lang}`).then((mod) => mod.default as DictionaryType),
+    ),
+  )
+
+  return Object.fromEntries(
+    locales.map((lang, index) => [lang, dictionaries[index]]),
+  )
+}
+
+export const getDictionary = async (locale: DictionaryKeyType) => {
+  const dictionaries = await getDictionaries()
+
+  return dictionaries[locale] || dictionaries.en
+}
